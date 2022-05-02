0d9ea758de4117490b73ec9d538c31cee75e29f5
Albert Pichler <albertpichler@gmx.net>
2022-05-02 14:44:14 +0200

    Add ESlint stuff

--- src/components/ui/theme/Header/Navbar/index.tsx
+++ src/components/ui/theme/Header/Navbar/index.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     <Wrapper as={Container}>
       <Link href="/" passHref>
         <Brand as="a" theme={theme}>
-          Albert Pichler
+          John Doe
         </Brand>
       </Link>
       <NavbarLinks desktop />
